6c8bd9e974e7ec6463b5ee5c622dc53c71b8e11d
Wil-dev <mehdididou7@icloud.com>
2025-03-20 16:49:53 +0000

    modif Services.jsx fini

--- src/components/Services/ServicesBis.jsx
+++ src/components/Services/ServicesBis.jsx
@@ -4,12 +4,12 @@ import { FaFileInvoiceDollar, FaHandHoldingUsd } from "react-icons/fa";
 import { IoMdHappy } from "react-icons/io";
 import { MdSecurity } from "react-icons/md";
 import { PiVignette } from "react-icons/pi";
+import desktop from "../../assets/desktop.svg";
 import leoLogo from "../../assets/leo-logo.jpg";
 import lgpiLogo from "../../assets/logo-lgpi2.jpg";
 import pharmalandLogo from "../../assets/logo-pharmaland2.jpeg";
 import smartRxLogo from "../../assets/logo-smartRx.png";
 import winpharmaLogo from "../../assets/winpharma-logo.jpeg";
-import desktop from "../../assets/desktop.svg";
 
 export default function Services() {
 	const services = [
@@ -53,31 +53,31 @@ export default function Services() {
 	const softwares = [
 		{
 			id: 1,
-			title: "lgpi",
+			title: "LGPI",
 			link: "#",
 			icon: lgpiLogo,
 		},
 		{
 			id: 2,
-			title: "pharmaland",
+			title: "Pharmaland",
 			link: "#",
 			icon: pharmalandLogo,
 		},
 		{
 			id: 3,
-			title: "winpharma",
+			title: "Winpharma",
 			link: "#",
 			icon: winpharmaLogo,
 		},
 		{
 			id: 4,
-			title: "smartRx",
+			title: "SmartRx",
 			link: "#",
 			icon: smartRxLogo,
 		},
 		{
 			id: 5,
-			title: "leo",
+			title: "Leo",
 			link: "#",
 			icon: leoLogo,
 		},
@@ -87,54 +87,57 @@ export default function Services() {
 		<section className="bg-light min-h-[1000px]">
 			<div className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-8 pt-20">
 				{/* Services */}
-				<h1 className="text-4xl text-center lg:text-left xl:text-[50px] md:text-5xl font-bold !leading-snug text-mint-dark mb-20">
-					Nos services
-				</h1>
-				<div className="grid grid-cols-2 md:grid-cols-3 sm:grid-cols-3 lg:grid-cols-6 gap-8">
-					{services.map((service) => (
-						<div
-							key={service.id}
-							className="bg-mint/20 rounded-2xl  flex flex-col gap-4 items-center justify-center p-4 py-7 hover:bg-light/90 hover:scale-110 duration-300 hover:shadow-2xl"
-						>
-							<div className="text-4xl mb-4">{service.icon}</div>
-							<h2 className="text-lg font-semibold text-center px-3">
-								{service.title}
-							</h2>
-						</div>
-					))}
-				</div>
-				{/* Logiciels */}
-				<h1 className="text-2xl text-center lg:text-left xl:text-[40px] md:text-4xl font-bold !leading-snug text-mint-dark mt-20">
-					Spécialisés dans les logiciels :
-				</h1>
-				<div className="containers mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 px-4 sm:px-6 md:px-8 py-14 md:py-10">
-					{/* Logiciels items */}
-					<div className="flex flex-col gap-8 justify-center items-center lg:items-start w-full mx-auto lg:ml-0 mb-5">
-						{softwares.map((software) => (
+				<div className="mb-10">
+					<h1 className="text-4xl text-center lg:text-left xl:text-[50px] md:text-5xl font-bold !leading-snug text-mint-dark mb-20">
+						Nos services
+					</h1>
+					<div className="grid grid-cols-2 md:grid-cols-3 sm:grid-cols-3 lg:grid-cols-6 gap-4 xl:gap-8">
+						{services.map((service) => (
 							<div
-								key={software.id}
-								className="bg-mint/20 rounded-2xl flex flex-row gap-4 items-center justify-start p-2 py-2 w-full max-w-md hover:bg-light/90 hover:scale-110 duration-300 hover:shadow-2xl"
+								key={service.id}
+								className="bg-mint/20 rounded-2xl  flex flex-col gap-4 items-center justify-center p-4 py-7 hover:bg-light/90 hover:scale-110 duration-300 hover:shadow-2xl"
 							>
-								<Image
-									src={software.icon}
-									alt={software.title}
-									width={50}
-									height={50}
-									className="rounded-xl"
-								/>
+								<div className="text-4xl mb-4">{service.icon}</div>
 								<h2 className="text-lg font-semibold text-center px-3">
-									{software.title}
+									{service.title}
 								</h2>
 							</div>
 						))}
 					</div>
-					{/* Logiciels Image */}
-					<div className="flex items-center justify-center w-full max-w-xl lg:w-[100%] md:w-[80%] md:my-10 xl:max-w-4xl mx-auto  xl:pl-30">
-						<Image
-							src={desktop}
-							alt="ordi"
-							className="w-full max-w-md md:max-w-xl xl:w-[600px] h-auto"
-						/>
+				</div>
+				{/* Logiciels */}
+				<div>
+					<h1 className="text-2xl text-center lg:text-left xl:text-[40px] md:text-4xl font-bold !leading-snug text-mint-dark mt-20">
+						Spécialisés dans les logiciels :
+					</h1>
+					<div className="containers mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 px-4 sm:px-6 md:px-8 py-14 md:py-10">
+						{/* Logiciels items */}
+						<div className="flex flex-col gap-4 xl:gap-8 justify-center items-center lg:items-start w-full mx-auto lg:ml-0 mb-5">
+							{softwares.map((software) => (
+								<div
+									key={software.id}
+									className="bg-mint/20 rounded-2xl flex flex-row gap-4 items-center justify-start px-2 py-2 w-full h-20 max-w-md hover:bg-light/90 hover:scale-110 duration-300 hover:shadow-2xl"
+								>
+									<Image
+										src={software.icon}
+										alt={software.title}
+										
+										className="rounded-full w-16 h-16 object-cover border-2 border-white"
+									/>
+									<h2 className="text-lg font-semibold text-center px-4">
+										{software.title}
+									</h2>
+								</div>
+							))}
+						</div>
+						{/* Logiciels Image */}
+						<div className="flex items-center justify-center w-full max-w-xl lg:w-[100%] md:w-[80%] md:my-10 xl:max-w-4xl mx-auto  xl:pl-30">
+							<Image
+								src={desktop}
+								alt="ordi"
+								className="w-full max-w-md md:max-w-xl xl:w-[600px] h-auto"
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
