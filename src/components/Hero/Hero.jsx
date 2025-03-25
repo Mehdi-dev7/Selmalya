7ed80b530252f7e2ccd7ed7b1243e0c923635a7a
Wil-dev <mehdididou7@icloud.com>
2025-03-25 22:31:01 +0000

    Amélioration du composant Hero.jsx avec des ajustements des classes CSS pour optimiser la mise en page et l'alignement des éléments. Les dimensions des images et des conteneurs ont été modifiées pour une meilleure réactivité et fluidité visuelle.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -34,20 +34,20 @@ export default function Hero() {
 
 	return (
 		<section className="bg-light min-h-[800px] sm:min-h-screen flex items-center justify-center py-10 sm:py-16">
-			<div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 px-4 sm:px-6 md:px-8">
+			<div className="container mx-auto grid grid-cols-1 md:grid-cols-2 2xl:space-x-50 gap-16 lg:gap-15 xl:gap-30 px-4 sm:px-6 md:px-10 mt-10 lg:mt-0">
 				{/* Blob et Image */}
-				<div className="flex justify-center items-center">
+				<div className="flex justify-center items-center px-4 md:px-10">
 					<MotionImage
 						src={Blob}
 						alt="blob"
-						className="absolute w-[550px] md:w-[570px] lg:w-[700px] xl:w-[800px] 2xl:w-[1000px] opacity-70"
+						className="absolute w-[580px] md:w-[600px] lg:w-[700px] xl:w-[800px] 2xl:w-[900px] opacity-70"
 						initial={{ x: -50, opacity: 0 }}
 						animate={{ x: 0, opacity: 1 }}
 						transition={{ duration: 0.6, delay: 0.2, ease: "easeInOut" }}
 					/>
 
 					<motion.div
-						className="relative w-[200px] md:w-[250px] lg:w-[300px] xl:w-[350px] 2xl:w-[400px] h-[200px] md:h-[250px] lg:h-[300px] xl:h-[350px] 2xl:h-[400px] rounded-full overflow-hidden border-4 border-white shadow-lg"
+						className="relative w-[170px] md:w-[250px] lg:w-[300px] xl:w-[350px] 2xl:w-[400px] h-[170px] sm:h-[250px] md:h-[250px] lg:h-[300px] xl:h-[350px] 2xl:h-[400px] rounded-full overflow-hidden border-4 border-white shadow-lg"
 						initial={{ x: 50, opacity: 0 }}
 						animate={{ x: 0, opacity: 1 }}
 						transition={{ duration: 0.6, delay: 0.4, ease: "easeInOut" }}
@@ -61,9 +61,9 @@ export default function Hero() {
 				</div>
 
 				{/* Texte */}
-				<div className="flex flex-col items-center md:items-start text-center md:text-left space-y-5">
+				<div className="flex flex-col items-center md:items-start text-center md:text-left space-y-6 px-4 md:px-8 mt-10 lg:mt-0 2xl:mt-10">
 					<motion.h1
-						className="text-5xl md:text-6xl lg:text-8xl xl:text-8xl 2xl:text-9xl font-bold"
+						className="text-5xl md:text-6xl lg:text-[80px] xl:text-8xl 2xl:text-9xl font-bold"
 						initial="initial"
 						animate="animate"
 						variants={FadeUp(0.5)}
@@ -90,7 +90,7 @@ export default function Hero() {
 							offset={-100}
 							duration={500}
 							onClick={handleContactClick}
-							className="primary-btn flex items-center gap-2 mt-5 md:mt-10 px-10 py-3 text-lg md:text-xl lg:text-2xl cursor-pointer hover:shadow-xl hover:scale-101 transition-all"
+							className="primary-btn flex items-center gap-2 mt-5 md:mt-15 px-10 py-3 text-lg md:text-xl lg:text-2xl cursor-pointer hover:shadow-xl hover:scale-101 transition-all"
 						>
 							Contactez-nous
 						</ScrollLink>
