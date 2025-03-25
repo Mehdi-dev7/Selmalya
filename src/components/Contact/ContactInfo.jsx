101f45bf1e673d137a31c44e34d60213444ae6bc
Wil-dev <mehdididou7@icloud.com>
2025-03-25 00:27:35 +0000

    Ajout d'un style pour cacher le débordement horizontal du corps et mise à jour des classes de style dans le composant ContactInfo pour améliorer l'apparence et l'interaction des liens.

--- src/components/Contact/ContactInfo.jsx
+++ src/components/Contact/ContactInfo.jsx
@@ -9,15 +9,15 @@ export default function ContactInfo() {
 		initial={{ opacity: 0, y: 40 }}
 		whileInView={{ opacity: 1, y: 0 }}
 		viewport={{ once: true, amount: 0.3 }}
-		transition={{ duration: 0.4,delay: 0.7, ease: "easeOut" }}
-		className="flex flex-col gap-4 font-semibold text-light">
+		transition={{ duration: 0.4,delay: 0.6, ease: "easeOut" }}
+		className="flex flex-col gap-4 font-semibold text-mint-dark">
 			{/* Email avec lien mailto */}
 			<div className="flex items-center gap-4 justify-start">
 				<HiOutlineMail className="text-3xl" />
 				<a 
 					href="mailto:selmalya.tierspayant@gmail.com" 
 					target="_blank"
-					className="hover:underline transition-all duration-200 hover:text-mint-dark"
+					className="hover:underline transition-all duration-200 hover:text-light"
 				>
 					selmalya.tierspayant@gmail.com
 				</a>
@@ -28,7 +28,7 @@ export default function ContactInfo() {
 				<FiPhone className="text-3xl" />
 				<a 
 					href="tel:+33184210733"
-					className="hover:underline transition-all duration-200 hover:text-mint-dark"
+					className="hover:underline transition-all duration-200 hover:text-light"
 				>
 					+33 1 84 21 07 33
 				</a>
@@ -41,7 +41,7 @@ export default function ContactInfo() {
 					href="https://wa.me/33664769230" 
 					target="_blank" 
 					rel="noopener noreferrer"
-					className="hover:underline transition-all duration-200 hover:text-mint-dark"
+					className="hover:underline transition-all duration-200 hover:text-light"
 				>
 					+33 6 64 76 92 30
 				</a>
@@ -54,7 +54,7 @@ export default function ContactInfo() {
 					href="https://maps.google.com/?q=7,+allée+André+Malraux,+93430+villetaneuse,+France" 
 					target="_blank" 
 					rel="noopener noreferrer"
-					className="hover:underline transition-all duration-200 hover:text-mint-dark"
+					className="hover:underline transition-all duration-200 hover:text-light"
 				>
 					7, allée André Malraux, 93430 villetaneuse
 				</a>
