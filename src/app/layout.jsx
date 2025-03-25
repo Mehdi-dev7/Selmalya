aa76992202854aeb7079e45bf7a4cc8870242617
Wil-dev <mehdididou7@icloud.com>
2025-03-25 00:46:11 +0000

    Ajout de styles CSS pour améliorer le comportement de défilement et la présentation du corps, incluant un ajustement de la hauteur minimale et des propriétés de police. Mise à jour du composant layout.jsx pour inclure une balise meta viewport, optimisant ainsi l'affichage sur les appareils mobiles.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -84,8 +84,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="fr">
+			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			</head>
 			<body
-				className={`${geistSans.variable} ${poppins.variable} ${inter.variable}  antialiased`}
+				className={`${geistSans.variable} ${poppins.variable} ${inter.variable} antialiased`}
 			>
 				{children}
 			</body>
