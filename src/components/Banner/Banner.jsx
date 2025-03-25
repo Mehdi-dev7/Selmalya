f4858a63a307270978b539dbb913ad59873d57e9
Wil-dev <mehdididou7@icloud.com>
2025-03-25 17:25:32 +0000

    Amélioration du composant Banner.jsx avec des ajustements des classes CSS pour une meilleure mise en page et alignement des éléments. Les animations ont été optimisées pour une expérience utilisateur plus fluide, notamment en modifiant les délais et les propriétés de transition.

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,14 +7,14 @@ export default function Banner() {
 		<section className="bg-light/50 min-h-[800px] sm:min-h-[1000px] flex items-center justify-center py-10 sm:py-16">
 			<div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 px-4 sm:px-6 md:px-8 py-8 md:py-24">
 				{/* banner text */}
-				<div className="flex flex-col justify-center items-center lg:items-start w-full max-w-xl mx-auto lg:mx-0">
-					<div className="text-center lg:text-left space-y-6">
+				<div className="flex flex-col justify-center items-center lg:items-start w-full max-w-xl mx-auto order-2 lg:order-1">
+					<div className="text-center lg:text-left space-y-4 sm:space-y-6">
 						<motion.h1
-							className="text-4xl xl:text-[50px] md:text-5xl font-bold !leading-snug text-mint-dark mb-20"
-							initial={{ y: 50, opacity: 0 }}
-							whileInView={{ y: 0, opacity: 1 }}
+							className="text-3xl sm:text-4xl xl:text-[50px] md:text-5xl font-bold leading-tight text-mint-dark mb-8 sm:mb-12 md:mb-16"
+							initial={{ opacity: 0, y: 20 }}
+							whileInView={{ opacity: 1, y: 0 }}
 							viewport={{ amount: 0.3, once: true }}
-							transition={{ duration: 0.6, delay: 0.2, ease: "easeInOut" }}
+							transition={{ duration: 0.5 }}
 						>
 							Qui sommes-nous ?
 						</motion.h1>
@@ -30,10 +30,10 @@ export default function Banner() {
 						</motion.h2>
 						<motion.p
 							className="text-base sm:text-lg md:text-xl leading-relaxed"
-							initial={{ y: 50, opacity: 0 }}
-							whileInView={{ y: 0, opacity: 1 }}
+							initial={{ opacity: 0, y: 20 }}
+							whileInView={{ opacity: 1, y: 0 }}
 							viewport={{ amount: 0.3, once: true }}
-							transition={{ duration: 0.6, delay: 0.6, ease: "easeInOut" }}
+							transition={{ duration: 0.6, delay: 0.3 }}
 						>
 							Avec 20 ans d'expérience en pharmacie, Selmalya est le partenaire
 							de confiance des officines pour l'externalisation du tiers payant.
@@ -46,16 +46,16 @@ export default function Banner() {
 
 				{/* banner image */}
 				<motion.div
-					className="flex justify-center items-center w-full max-w-xl xl:max-w-4xl mx-auto lg:mx-0"
-					initial={{ x: 50, opacity: 0 }}
-					whileInView={{ x: 0, opacity: 1 }}
+					className="flex justify-center items-center w-full max-w-xl mx-auto lg:pl-8 order-1 lg:order-2"
+					initial={{ opacity: 0, x: 50 }}
+					whileInView={{ opacity: 1, x: 0 }}
 					viewport={{ amount: 0.3, once: true }}
-					transition={{ duration: 0.6, delay: 0.4, ease: "easeInOut" }}
+					transition={{ duration: 0.6, ease: "easeOut" }}
 				>
 					<Image
 						src={BannerImage}
-						alt="banner"
-						className="w-full max-w-md md:max-w-xl xl:w-[600px] h-auto"
+						alt="Selmalya description"
+						className="w-full max-w-[280px] sm:max-w-md md:max-w-lg lg:w-[100%] h-auto"
 					/>
 				</motion.div>
 			</div>
